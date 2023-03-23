1cc318240af40762652dd2a8adca0c37c9149f65
KarlJosephKumar <kjkumr@gmail.com>
2023-03-23 14:46:42 +0200

    philo.h updated, added common and some items to philo struct

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: kakumar <kakumar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 14:43:42 by kakumar           #+#    #+#             */
-/*   Updated: 2023/03/22 16:05:53 by kakumar          ###   ########.fr       */
+/*   Updated: 2023/03/23 14:44:20 by kakumar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,43 @@
 # define PHILO_H
 
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+typedef struct s_common
+{
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			number_of_times_to_eat;
+}	t_common;
 
 typedef struct s_philo
 {
-	int		index;
-	int		times_eaten;
-	int		is_sleeping;
-	int		is_eating;
-	int		is_dead;
-	t_data *data;
+	int				index;
+	int				times_eaten;
+	int				last_meal;
+	int				state;
+	pthread_mutex_t fork;
+	long long		start_time;
+	t_common		*common;
 }	t_philo;
 
 typedef struct s_data
 {
-	t_philo	*philo;
-	int		number_of_philosophers;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		number_of_times_to_eat;
+	t_philo		*philo;
+	pthread_t	*threads;
+	t_common	*common;
+	int			number_of_philosophers;
 }	t_data;
 
+//threads
+void	run_threads(t_data *data);
 //initialize
-void	init_data_philos(t_data *data, char **argv);
+void	init_data_philos(t_data *data, char **argv, t_common *common);
 //utils
 int		philo_atoi(char *str);
+long long get_time_in_ms(void);
+
 #endif
